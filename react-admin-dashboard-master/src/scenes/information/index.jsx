c9373da8eed49258384417c4eb8da64b4ffce65b
RajPanganamala <84967860+Raj2k20@users.noreply.github.com>
2022-12-06 07:34:59 +0530

    Update index.jsx

--- react-admin-dashboard-master/src/scenes/information/index.jsx
+++ react-admin-dashboard-master/src/scenes/information/index.jsx
@@ -1,16 +1,22 @@
 import axios from "axios";
-import {useState, useEffect } from "react";
-// import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-// import Header from "../../components/Header";
-import { useTheme,Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import Header from "../../components/Header";
+import { useTheme, Box } from "@mui/material";
 import { tokens } from "../../theme";
 const Information = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
-    { field: "id", headerName: "ID"},
-    { field: "plates", headerName: "NumberPlate",type: "number",flex: 0.5,headerAlign: "center",
-    align: "center", },
+    { field: "id", headerName: "ID" },
+    {
+      field: "plates",
+      headerName: "NumberPlate",
+      type: "number",
+      flex: 0.5,
+      headerAlign: "center",
+      align: "center",
+    },
     {
       field: "timestamp",
       headerName: "timestamp",
@@ -30,56 +36,34 @@ const Information = () => {
     {
       field: "location",
       headerName: "Location",
-      type: "string", 
+      type: "string",
       flex: 1,
       headerAlign: "center",
       align: "center",
     },
-    
   ];
-const stringObj1 = {};
-const arr = [];
- var [ccdata, setccdata] = useState([]);
- 
-//  setInterval(() => {
-	axios.get("http://localhost:4000/fetchall").then((response) => {
-          setccdata(response.data);
-          console.log(response.data);
-          // stringObj1 = JSON.stringify(ccdata);
-          // arr =[...stringObj1];
-         }).catch(function (error) {console.log("error here3");});
-// }, 5000);
-  // const [ccdata,setccdata]=useState() 
-  // useEffect(()=>{ 
-  // var request = new XMLHttpRequest(); 
-  // request.onreadystatechange = function() { 
-  // if (request.readyState === 4 && request.status === 200) { 
-  // const response=JSON.parse(request.response) 
-  // setccdata(response) 
-  // console.log(response)
-  // console.log("Third log");
-  // } 
-  // }; 
-  // request.open('GET','http://localhost:4000/fetchall', true); 
-  // request.send(); 
-  // },[]) 
-  // useEffect(()=>{ 
-  // console.log(ccdata) 
-  // console.log("Fourth log");
-  // },[ccdata]) 
+  const stringObj1 = {};
+  const arr = [];
+  var [ccdata, setccdata] = useState([]);
+
+  //  setInterval(() => {
+  async function fetchItems() {
+    axios
+      .get("http://localhost:4000/fetchall")
+      .then(async (response) => {
+        const jsos = await response.data;
+        setccdata(jsos);
+        console.log("HI1");
+      })
+      .catch(function (error) {
+        console.log("error here3");
+        console.log(error);
+      });
+  }
+  fetchItems();
   return (
-    // <p>{}</p>
-    <div>{ccdata}</div>
-    // <div>{arr.map(arr => <div>{arr.id}</div>)}</div>
-    // <div>
-    //   <ol>
-    //   {ccdata.map((datas) => (
-    //     <li>{datas}</li>
-    //   ))}
-    // </ol>
-    // </div>
-    
-    /* <Box m="20px">
+    // <div>{ccdata && ccdata.map((ccdata,index)=><li key={index}>{ccdata.plates}</li>)}</div>
+    <Box m="20px">
       <Header
         title="CC Information"
         subtitle="List of data from the fog nodes"
@@ -122,7 +106,7 @@ const arr = [];
           components={{ Toolbar: GridToolbar }}
         />
       </Box>
-    </Box> */
+    </Box>
     /* <h1>{ccdata}</h1> */
   );
 };
